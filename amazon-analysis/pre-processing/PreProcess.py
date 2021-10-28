60bbc631597e0e2aae3d27f1998f7e155da550e6
Du Hoang <hoangminhdu2000@gmail.com>
2021-10-28 17:50:05 +0700

    update

--- amazon-analysis/pre-processing/PreProcess.py
+++
@@ -1,6 +0,0 @@
-import csv
-import pandas as pd
-
-# loading data
-data = pd.read_csv("../../amazon-product-api/crawl/demo_data.csv")
-print(data)
---
+++ amazon-analysis/preprocess.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# loading data
+data = pd.read_csv("../amazon-product-api/crawl/demo_data.csv")
+print(data)
