3360138599d7905c8a8d56da253122492d9fe8e2
Du Hoang <hoangminhdu@gmail.com>
2021-11-07 09:58:56 +0700

    preprocess

--- amazon-analysis/preprocess.py
+++ amazon-analysis/preprocess.py
@@ -1,7 +1,42 @@
 import pandas as pd
+from stop_words import get_stop_words
+from spellchecker import SpellChecker as spell_checker
+import string
+from nltk.stem import *
 
-# loading data
-data = pd.read_csv("../amazon-product-api/crawl/demo_data.csv")
-print(data)
-print(data["review.id"])
-print(data["review.title"])
+
+# Initialization
+spell = spell_checker()
+stop_words = get_stop_words('english')
+stemmer = SnowballStemmer("english")
+
+# Loading data
+df = pd.read_csv("../amazon-product-api/crawl/10rows_demo_data.csv")
+
+# Remove null
+df = df[df["review.title"].notnull()]
+
+# Correct review title
+for label, sentence in df["review.title"].items():
+    new_sentence = ""
+    # Filter Out Punctuation
+    punctuated_sentence = sentence.translate(
+        str.maketrans('', '', string.punctuation))
+    # Split into words
+    arr_sentence = punctuated_sentence.split()
+    for word in arr_sentence:
+        # Spelling correctionz
+        correct_word = spell.correction(word).lower()
+        # Stem word with filter out stop words
+        stemed_word = stemmer.stem(correct_word)
+        # Word correction one more time cause stem word library is not that good
+        validated_word = spell.correction(stemed_word).lower()
+        # Filter out stop words one more time
+        if not validated_word in stop_words:
+            # Remove duplicate word
+            if new_sentence.find(validated_word) == -1:
+                new_sentence += validated_word + ", "
+    df.loc[label, "review.title"] = new_sentence
+
+# Review
+print(df["review.title"])
