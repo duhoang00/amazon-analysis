0d2b9f8597ea00b8fc3f5cab743b78b8df3ad8e8
Du HHoang <hoangminhdu2000@gmail.com>
2021-11-23 13:11:34 +0700

    sentiment column

--- analysis/label.py
+++ analysis/label.py
@@ -2,15 +2,37 @@
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-df = pd.read_csv('./data/data-no-cr.csv')
-print(df['review.review'])
+
+def sentiment_label(row):
+    max_label = "positive"
+    max_value = row["pos"]
+    if (row["neg"] > max_value):
+        max_label = "negative"
+        max_value = row["neg"]
+    if (row["neu"] > max_value):
+        max_label = "neutral"
+        max_value = row["neu"]
+
+    return max_label
+
+
+df = pd.read_csv('./data/10rows_demo_data.csv')
 
 analyzer = SentimentIntensityAnalyzer()
 
-sentiment = df['review.title'].apply(analyzer.polarity_scores)
+sentiment = df['review.review'].apply(analyzer.polarity_scores)
 
 sentiment_df = pd.DataFrame(sentiment.tolist())
 
-print(sentiment_df)
 
-sentiment_df.to_csv("test.csv")
+label_df = df.merge(sentiment_df, left_index=True,
+                    right_index=True, suffixes=[False, False])
+
+label_df = label_df.drop(columns="compound")
+
+label_df['sentiment'] = label_df.apply(sentiment_label, axis=1)
+
+print("label_df")
+print(label_df)
+
+label_df.to_csv("label-data.csv")
